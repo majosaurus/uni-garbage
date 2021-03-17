97ae6e0c697afa1b0871748a045bbdbf57937787
Martina Koutová <koutova.maja@outlook.com>
2021-03-17 09:36:00 +0200

    #5 Assignment 1.2

--- machine_learning/workshop_exercise_01_02.py
+++ machine_learning/workshop_exercise_01_02.py
@@ -12,25 +12,6 @@
 #>>> print(sorted(vsequences))
 #['aiuio', 'eaiou', 'eouio', 'euoia', 'oauaio', 'uiieioa']
 
-"""
 words = ['attribution', 'confabulation', 'elocution', 'sequoia', 'tenacious', 'unidirectional']
-vsequences = set()
-for word in words:
-    vowels = []
-    for char in word:
-        if char in 'aeiou':
-            vowels.append(char)
-    print(vowels)
-    vsequences.add(''.join(vowels))
-    print(vsequences)
+vsequences = sorted(''.join(char for char in word if char in 'aeiyou') for word in words)
 print(vsequences)
-print(sorted(vsequences))
-"""
-words = ['attribution', 'confabulation', 'elocution', 'sequoia', 'tenacious', 'unidirectional']
-sequences = set()
-t = [{c for c in words for c in c if c in 'aeiyou'} for c in words]
-print(t)
-t = [[''+c for c in w if c in 'aeiyou'] for w in words]
-print(t)
-t = [c for c in words for c in c if c in 'aeiyou']
-print(t)
